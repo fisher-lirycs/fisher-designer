fd78e943aefcc51ad2813c14a69da45b637b3685
usyuuryou <usyuuryou@163.com>
2023-03-01 19:02:33 +0800

    scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
-import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles["App-logo"]} alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <h1>aaaa</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className={styles["App-link"]}
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
